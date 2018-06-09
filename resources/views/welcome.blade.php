7ed908cba82cb2e49551eab7c3ccec3b66d1600e
cavalvy <657288378@qq.com>
2018-06-09 17:54:00 +0800

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - {{ getenv('DB_DATABASE') }}
                 </div>
 
                 <div class="links">
