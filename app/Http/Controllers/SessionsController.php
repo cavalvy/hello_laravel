0a629e34cc9bdf2f660fed795ef7ddd067535aad
vagrant <vagrant@homestead>
2018-06-20 10:21:56 +0000

    优化

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -45,7 +45,7 @@ class SessionsController extends Controller
             }
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back();
+            return redirect()->back()->withInput();
         }
     }
 
