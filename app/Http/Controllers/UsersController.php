9cbfcb11e3aadfaf4f087cb16ffbb32ed496c08f
vagrant <vagrant@homestead>
2018-06-21 09:02:51 +0000

    yh

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,6 +47,7 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
+        dd(redirect('/')->getTargetUrl());
         $this->validate($request,[
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',//正对数据库表users
@@ -61,7 +62,7 @@ class UsersController extends Controller
         $this->sendEmailConfirmationTo($user);
         session()->flash('success','验证邮件已发送到你的注册邮箱上，请注意查收。');
 
-        return redirect()->route('home');
+        return redirect('/');
     }
 
     public function edit(User $user)
